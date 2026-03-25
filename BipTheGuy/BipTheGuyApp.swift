e6319d5842eef33b8d913deec10c339f74c1d03e
Robin Burrage <robin.o.burrage@gmail.com>
2026-03-25 10:16:31 -0400

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Robin Burrage on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
