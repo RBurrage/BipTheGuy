cb65d65516aa0086f5838203d415d71256015ffd
Robin Burrage <robin.o.burrage@gmail.com>
2026-03-25 12:07:10 -0400

    Add images and sounds and import AVFAudio to play sound on tap gesture

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,16 +6,45 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    
+    @State private var audioPlayer: AVAudioPlayer!
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image("clown")
+                .resizable()
+                .scaledToFit()
+                .onTapGesture {
+                    playSound(soundName: "punchSound")
+                }
+            Spacer()
+            Button {
+                // TODO: BUTTON ACTION HERE
+            } label: {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            }
         }
         .padding()
+    }
+    
+    func playSound(soundName: String) {
+        if audioPlayer != nil && audioPlayer.isPlaying {
+            audioPlayer.stop()
+        }
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("😡 Could not read file named \(soundName)")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+        }
     }
 }
 
